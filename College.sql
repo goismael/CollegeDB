fdad67221c31b8dde0c1010b7673fd690cfeaf17
Ismael <42499036+goismael@users.noreply.github.com>
2018-10-09 11:26:50 -0400

    Add files via upload

---
+++ College.sql
@@ -0,0 +1,31 @@
+create database College;
+use College
+go 
+create table Students 
+(ID int identity(1,1) Primary Key Clustered,
+FName varchar(10),
+LName varchar(10),
+SSN varchar(10),
+Address varchar(39),
+City varchar (25),
+State nvarchar(14),
+Zip int,
+Phone bigint
+)
+
+create table Classes
+(ID int identity(1,1) Primary Key Clustered,
+Title varchar (10),
+Number nvarchar (4),
+department nvarchar (20)
+)
+
+create table Scores
+(ID int identity(1,1) Primary key clustered,
+Type nvarchar (25),
+DateAssigned datetime,
+DateDue datetime,
+DateSubmitted datetime,
+PointsEraned int,
+PointPossible int 
+)
